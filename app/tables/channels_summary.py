3adff6ab6d7a5b3de28f6916aa8039afe6cf0e70
admin <timofeyegorov48@gmail.com>
2022-01-14 16:39:25 +0700

    change channels_summary

--- app/tables/channels_summary.py
+++ app/tables/channels_summary.py
@@ -10,20 +10,29 @@ def calculate_channels_summary(df):
     # Получаем массив трафикологов в отфильтрованном датасете
     filtered_trafficologists = df['trafficologist'].unique()
     values = []
+    temp_ = []
+    temp_.append('СУММА:')
+    temp_.append(df.shape[0])
+    temp_.append(round(df['payment_amount'].sum()))
+    temp_.append(round(temp_[2] / temp_[1]))
+    temp_.append(round(df['channel_expense'].sum()))
+    temp_.append(round(temp_[1] * 250 + temp_[2] * 0.35))
+    temp_.append(round(temp_[2] - temp_[4] - temp_[5]))
+    temp_.append(round((temp_[2] / (temp_[4] + temp_[5]) - 1) * 100))
+    temp_.append(round(temp_[7] / (1 + temp_[7]) * 100))
+    values.append(temp_)
     for el in filtered_trafficologists:
         temp_ = []
         temp_.append(el)
         temp_.append(df[df['trafficologist'] == el].shape[0])  # Кол-во лидов
-        temp_.append(df[df['trafficologist'] == el]['payment_amount'].sum())  # Оборот
-        temp_.append(round(temp_[2] / temp_[1], 1))  # Оборот на лида
-        temp_.append(round(df[df['trafficologist'] == el]['channel_expense'].sum(), 1))  # Трафик
-        temp_.append(temp_[1] * 250 + temp_[2] * 0.35)  # Остальное
-        temp_.append(round(temp_[2] - temp_[4] - temp_[5], 1))  # Прибыль
-        temp_.append(round((temp_[3] / (temp_[4] + temp_[5]) - 1) * 100, 1))  # ROI
-        temp_.append(round(temp_[7] / (1 + temp_[7]) * 100, 1))  # Маржинальность
+        temp_.append(round(df[df['trafficologist'] == el]['payment_amount'].sum()))  # Оборот
+        temp_.append(round(temp_[2] / temp_[1]))  # Оборот на лида
+        temp_.append(round(df[df['trafficologist'] == el]['channel_expense'].sum()))  # Трафик
+        temp_.append(round(temp_[1] * 250 + temp_[2] * 0.35))  # Остальное
+        temp_.append(round(temp_[2] - temp_[4] - temp_[5]))  # Прибыль
+        temp_.append(round((temp_[2] / (temp_[4] + temp_[5]) - 1) * 100))  # ROI
+        temp_.append(round(temp_[7] / (1 + temp_[7]) * 100))  # Маржинальность
         values.append(temp_)
-        # for i in range(len(temp_)):
-        #     print(i, temp_[i])
     output_df = pd.DataFrame(columns=['Канал', 'Лидов', 'Оборот*', 'Оборот на лида', 'Трафик', 'Остальное', 'Прибыль', 'ROI',
                                'Маржинальность'],
                       data=values)
