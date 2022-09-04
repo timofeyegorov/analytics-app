e41cab431ae0a71e5edde5f637c023b5da1f0dc8
BL146U <bl146u@gmail.com>
2022-09-04 13:49:41 +0300

    Сбор статистики ВК

--- app/tables/segments.py
+++ app/tables/segments.py
@@ -134,6 +134,17 @@ def calculate_segments(df):
                         | (df["account"] == traff_name)
                     )
                 ].shape[0]
+                print(df_category_val.loc[idx, traff_name])
+                print(
+                    df_category_val.loc[idx, traff_name]
+                    / (
+                        df[
+                            (df["trafficologist"] == traff_name)
+                            | (df["account"] == traff_name)
+                        ].shape[0]
+                    )
+                    * 100
+                )
                 df_category.loc[idx, traff_name] = (
                     (
                         df_category_val.loc[idx, traff_name]
