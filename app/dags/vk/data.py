f23625ea9fce38b94cfd2be64215cbcccdb7391a
BL146U <bl146u@gmail.com>
2022-09-04 17:16:36 +0300

    Сбор статистики ВК

--- app/dags/vk/data.py
+++ app/dags/vk/data.py
@@ -125,6 +125,106 @@ class CampaignAdFormatEnum(Enum):
         return CampaignAdFormatTitleEnum[self.name].value
 
 
+class AdFormatTitleEnum(Enum):
+    _1 = "Изображение и текст"
+    _2 = "Большое изображение"
+    _3 = "Эксклюзивный формат"
+    _4 = "Продвижение сообществ или приложений, квадратное изображение"
+    _5 = "Приложение в новостной ленте(устаревший)"
+    _6 = "Мобильное приложение"
+    _9 = "Запись в сообществе"
+    _11 = "Адаптивный формат"
+    _12 = "Истории"
+
+
+class AdFormatEnum(Enum):
+    _1 = 1
+    _2 = 2
+    _3 = 3
+    _4 = 4
+    _5 = 5
+    _6 = 6
+    _9 = 9
+    _11 = 11
+    _12 = 12
+
+    @property
+    def title(self) -> str:
+        return AdFormatTitleEnum[self.name].value
+
+
+class AdCostTypeTitleEnum(Enum):
+    _0 = "Оплата за переходы"
+    _1 = "Оплата за показы"
+    _3 = "Оптимизированная оплата за показы"
+
+
+class AdCostTypeEnum(Enum):
+    _0 = 0
+    _1 = 1
+    _2 = 2
+
+    @property
+    def title(self) -> str:
+        return AdCostTypeTitleEnum[self.name].value
+
+
+class AdGoalTypeTitleEnum(Enum):
+    _1 = "Показы"
+    _2 = "Переходы"
+    _3 = "Отправка заявок"
+    _5 = "Вступления в сообщество"
+    _6 = "Добавление в корзину"
+    _7 = "Добавление в список желаний"
+    _8 = "Уточнение сведений"
+    _9 = "Начало оформления заказа"
+    _10 = "Добавление платёжной информации"
+    _11 = "Покупка"
+    _12 = "Контакт"
+    _13 = "Получение потенциального клиента"
+    _14 = "Запись на приём"
+    _15 = "Регистрация"
+    _16 = "Подача заявки"
+    _17 = "Использование пробной версии"
+    _18 = "Оформление подписки"
+    _19 = "Посещение страницы"
+    _20 = "Просмотр контента"
+    _21 = "Использование поиска"
+    _22 = "Поиск местонахождения"
+    _23 = "Пожертвование средств"
+    _24 = "Конверсия"
+
+
+class AdGoalTypeEnum(Enum):
+    _1 = 1
+    _2 = 2
+    _3 = 3
+    _5 = 5
+    _6 = 6
+    _7 = 7
+    _8 = 8
+    _9 = 9
+    _10 = 10
+    _11 = 11
+    _12 = 12
+    _13 = 13
+    _14 = 14
+    _15 = 15
+    _16 = 16
+    _17 = 17
+    _18 = 18
+    _19 = 19
+    _20 = 20
+    _21 = 21
+    _22 = 22
+    _23 = 23
+    _24 = 24
+
+    @property
+    def title(self) -> str:
+        return AdGoalTypeTitleEnum[self.name].value
+
+
 class AccountData(BaseModel):
     access_role: AccountAccessRoleEnum
     account_id: PositiveInt
@@ -160,3 +260,10 @@ class AdData(BaseModel):
     account_id: PositiveInt
     client_id: Optional[PositiveInt]
     id: PositiveInt
+    campaign_id: PositiveInt
+    ad_format: AdFormatEnum
+    cost_type: AdCostTypeEnum
+    cpc: PositiveInt
+    cpm: PositiveInt
+    ocpm: PositiveInt
+    goal_type: AdGoalTypeEnum
