66f8915af6909e653915619c2c7ee8100f5a5648
BL146U <bl146u@gmail.com>
2022-09-09 21:05:36 +0300

    Сбор статистики ВК

--- app/dags/vk/collector.py
+++ app/dags/vk/collector.py
@@ -55,6 +55,7 @@ def chr_convert(text: str) -> str:
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print("Encountered a start tag:", tag)
+        print("     attrs:", attrs)
 
     def handle_endtag(self, tag):
         print("Encountered an end tag :", tag)
