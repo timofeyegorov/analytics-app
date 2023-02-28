3fc441894df0b98452c591de62ecd4af195847b0
BL146U <bl146u@gmail.com>
2023-02-28 11:39:51 +0700

    Сбор еженедельной статистики по SpecialOffers

--- app/assets/js/week-stats/so/index.js
+++ app/assets/js/week-stats/so/index.js
@@ -7,8 +7,8 @@
     Array.prototype.sum = function() {
         return this.reduce((v1, v2) => {
             let sum = 0,
-                v1_parse = parseInt(v1),
-                v2_parse = parseInt(v2);
+                v1_parse = parseFloat(v1),
+                v2_parse = parseFloat(v2);
             if (!isNaN(v1_parse) && `${v1_parse}` === `${v1}`) sum += v1_parse;
             if (!isNaN(v2_parse) && `${v2_parse}` === `${v2}`) sum += v2_parse;
             return sum;
@@ -23,70 +23,51 @@
 
         let render_int = (value) => {
             let value_parse = parseFloat(value);
-            if (!isNaN(value_parse) && `${value_parse}` === `${value}`) return Math.ceil(value_parse).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+            if (!isNaN(value_parse) && `${value_parse}` === `${value}`)
+                return Math.round(value_parse).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
             return "0";
         };
 
         let recalculate = () => {
             let table = $("#statistics > table"),
                 td = table.find("tbody > tr > td"),
+                td_so = td.filter((_, item) => item.cellIndex === 3).slice(1),
+                td_profit = td.filter((_, item) => item.cellIndex === 4).slice(1),
+                td_values = td.filter((_, item) => item.cellIndex > 4 && item.parentElement.rowIndex > 1),
+                td_total = td.filter((_, item) => item.cellIndex > 2 && item.parentElement.rowIndex === 1),
                 is_accumulative = accumulative_field[0].checked,
-                is_profit = profit_field[0].checked,
-                zooms = 0;
+                is_profit = profit_field[0].checked;
 
-            td.filter((_, item) => item.cellIndex === 3 && item.parentElement.rowIndex === 1).each((_, item) => {
-                zooms = parseInt(item.dataset.value) / (is_accumulative ? td.filter((_, item) => item.cellIndex === 3).slice(1).length : 1);
-                item.innerText = render_int(zooms);
-            });
-
-            if (is_accumulative) {
-                td.filter((_, item) => item.cellIndex === 4).slice(1).each((_, item) => {
-                    let row_index = item.parentElement.rowIndex;
-                    item.innerText = render_int(parseInt(item.dataset.value) / (is_profit ? parseInt(td.filter((_, item) => item.cellIndex === 3 && item.parentElement.rowIndex === row_index)[0].dataset.value) : 1));
-                });
-                td.filter((_, item) => item.cellIndex > 4 && item.parentElement.rowIndex > 1 && `${item.dataset.value}` !== "").each((_, item) => {
-                    let row_index = item.parentElement.rowIndex,
-                        cell_index = item.cellIndex;
-                    item.innerText = render_int(td.filter((_, item) => item.cellIndex > 4 && item.cellIndex <= cell_index && item.parentElement.rowIndex === row_index).map((_, item) => item.dataset.value).toArray().sum() / (is_profit ? parseInt(td.filter((_, item) => item.cellIndex === 3 && item.parentElement.rowIndex === row_index)[0].dataset.value) : 1));
-                });
-                td.filter((_, item) => item.cellIndex === 4 && item.parentElement.rowIndex === 1). each((_, item) => {
-                    let row_index = item.parentElement.rowIndex,
-                        cell_index = item.cellIndex;
-                    item.innerText = render_int(parseInt(item.dataset.value) / td.filter((_, item) => item.cellIndex === cell_index && item.parentElement.rowIndex > 1 && `${item.dataset.value}` !== "").length / (is_profit ? zooms : 1));
-                });
-                td.filter((_, item) => item.cellIndex > 4 && item.parentElement.rowIndex === 1).each((_, item) => {
-                    let row_index = item.parentElement.rowIndex,
-                        cell_index = item.cellIndex;
-                    item.innerText = render_int(td.filter((_, item) => item.cellIndex > 4 && item.cellIndex <= cell_index && item.parentElement.rowIndex === row_index).map((_, item) => item.dataset.value).toArray().sum() / (is_profit ? parseInt(td.filter((_, item) => item.cellIndex === 3 && item.parentElement.rowIndex === 1)[0].dataset.value) : td.filter((_, item) => item.cellIndex > 4 && item.parentElement.rowIndex === 1).length));
-                });
-            } else {
-                td.filter((_, item) => item.cellIndex > 3 && `${item.dataset.value}` !== "").each((_, item) => {
-                    let row_index = item.parentElement.rowIndex;
-                    item.innerText = render_int(parseInt(item.dataset.value) / (is_profit ? parseInt(td.filter((_, item) => item.cellIndex === 3 && item.parentElement.rowIndex === row_index)[0].dataset.value) : 1));
-                });
-            }
-        };
-
-        let render_table = () => {
-            let table = $("#statistics > table"),
-                td = table.find("tbody > tr > td");
-
-            td.filter((_, item) => item.cellIndex === 3).slice(1).each((_, item) => {
+            td_so.each((_, item) => {
+                item.dataset.data = item.dataset.value;
                 $(item).text(render_int(item.dataset.value));
             });
 
-            td.filter((_, item) => item.cellIndex === 4).slice(1).each((_, item) => {
-                item.dataset.value = $(item.parentElement).children("td").slice(4).map((_, item) => item.dataset.value).toArray().sum();
+            td_profit.each((_, item) => {
+                let row = $(item.parentElement).children("td").slice(4),
+                    row_td_so = $(item.parentElement).children("td")[2],
+                    divider = is_profit ? parseInt(row_td_so.dataset.value) : 1;
+                item.dataset.data = row.map((_, item) => item.dataset.value).toArray().sum() / divider;
+                $(item).text(render_int(item.dataset.data));
             });
 
-            td.filter((_, item) => item.parentElement.rowIndex === 1).slice(2).each((_, item) => {
-                let cell_index = item.cellIndex;
-                item.dataset.value = td.filter((_, item) => item.cellIndex === cell_index).slice(1).map((_, item) => item.dataset.value).toArray().sum();
+            td_values.each((_, item) => {
+                let row_td_so = $(item.parentElement).children("td")[2],
+                    divider = is_profit ? parseInt(row_td_so.dataset.data) : 1,
+                    value = is_accumulative ? $(item.parentElement).children("td").filter((_, td) => td.cellIndex > 4 && td.cellIndex <= item.cellIndex).map((_, item) => item.dataset.value).toArray().sum() : item.dataset.value;
+                item.dataset.data = `${item.dataset.value}` !== "" ? parseInt(value) / divider : "";
+                $(item).text(`${item.dataset.data}` !== "" ? render_int(item.dataset.data) : "");
             });
 
+            td_total.each((_, item) => {
+                let td_col = td.filter((_, td) => td.cellIndex === item.cellIndex && td.parentElement.rowIndex > 1);
+                $(item).text(render_int(td_col.map((_, item) => item.dataset.data).toArray().sum()));
+            });
+        };
+
+        let render_table = () => {
             recalculate();
-
-            table.removeClass("hidden");
+            $("#statistics > table").removeClass("hidden");
         };
 
         accumulative_field.bind("change", recalculate);
