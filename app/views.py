13a065739729417fc6496b5a5b2fbac769859ec7
BL146U <bl146u@gmail.com>
2023-03-03 19:09:41 +0700

    Webhook для получения лидов из тильды

--- app/views.py
+++ app/views.py
@@ -3563,7 +3563,7 @@ class TildaLeadsView(APIView):
         print(
             list(
                 map(
-                    lambda item: bytes(item, "iso-8859-1").decode("utf-8"),
+                    lambda item: bytes(item, "cp1251").decode("utf-8"),
                     data.keys(),
                 )
             )
