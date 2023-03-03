c10d3147bf4070fc44fae0cc3414732dce917fa2
BL146U <bl146u@gmail.com>
2023-03-03 18:42:50 +0700

    Webhook для получения лидов из тильды

--- app/views.py
+++ app/views.py
@@ -3560,7 +3560,9 @@ class TildaLeadsView(APIView):
     def post(self, *args, **kwargs):
         print("- POST --------------------")
         data = request.form.to_dict()
-        print(list(map(lambda item: item.encode("latin-1").decode("utf-8"), data.keys())))
+        print(
+            list(map(lambda item: item.encode("utf-8").decode("latin-1"), data.keys()))
+        )
         print(data.values())
         print("---------------------------")
         return super().post(*args, **kwargs)
