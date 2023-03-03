a1a574d758a4cfe3c9f0a00a8b7ac89d18355fec
BL146U <bl146u@gmail.com>
2023-03-03 16:34:04 +0700

    Страница поиска лидов в Тильде

--- app/assets/js/search-leads/index.js
+++ app/assets/js/search-leads/index.js
@@ -11,6 +11,18 @@
             $(event.currentTarget).parent().children(".extra").toggleClass("visible");
         });
 
+        new ClipboardJS("#search-leads-response > .item > * > .param > .value > i", {
+            text: (trigger) => {
+                let target = $(trigger).next("span");
+                target.parent().addClass("copied");
+                if (target[0].CopiedTimeout) clearTimeout(target[0].CopiedTimeout);
+                target[0].CopiedTimeout = setTimeout(() => {
+                    target.parent().removeClass("copied");
+                }, 500);
+                return target.text();
+            },
+        });
+
     });
 
 
