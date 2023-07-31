1111bd3e5209db4a4ec80eeaa56d024bda646744
Кирилл <kiryamiass7@gmail.com>
2023-07-31 10:31:40 +0500

    Обновлен функционал звонков

--- app/ats/preparData.py
+++ app/ats/preparData.py
@@ -3,12 +3,15 @@ import pandas as pd
 file_path = 'app/ats/api/data.csv'
 filter_open: list = []
 filter_num: list = []
+settings_open: list = []
+data_range = ''
 
 
 # Функция подготовки базовой таблицы для всех срезов
 def prep_data():
     global filter_num
     global filter_open
+    global settings_open
     data = pd.read_csv(file_path, delimiter=';', low_memory=False)
     # удалим лишние данные
     new_data = data.drop(
@@ -21,6 +24,8 @@ def prep_data():
     new_data['Звонок'] = 1
     # Это нужно чтобы работал query, с пробелами в названии не работает
     new_data = new_data.rename(columns={'Исходящая линия': 'ИсходящаяЛиния'})
+    if settings_open != []:
+        new_data = new_data.query(f'Откуда == {settings_open}')
     if filter_open != []:
         new_data = new_data.query(f'Откуда == {filter_open}')
     if filter_num != []:
@@ -39,8 +44,35 @@ def filter_openers(openers: list):
     filter_open = openers
 
 
+def settings_openers(openers: list):
+    global settings_open
+    settings_open = openers
+
+
 def filter_delete():
     global filter_open
     global filter_num
-    filter_open = []
+    global settings_open
+    filter_open = settings_open
     filter_num = []
+
+
+def settings_delete():
+    global settings_open
+    global filter_open
+    data = pd.read_csv(file_path, delimiter=';', low_memory=False)
+    data_set = data['Откуда'].tolist()
+    result = list(set(data_set))
+    settings_open = result
+    filter_open = result
+    return settings_open
+
+
+def set_datarange(data):
+    global data_range
+    data_range = data
+
+
+def show_datarange():
+    global data_range
+    return data_range
