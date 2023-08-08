df5bfff77096e00b2b1694135f3d97a2f7026875
BL146U <bl146u@gmail.com>
2023-08-08 15:28:42 +0700

    Обновленные пакеты

--- app/views_api_v1.py
+++ app/views_api_v1.py
@@ -14,7 +14,8 @@ from typing import Dict, Any
 from pathlib import Path
 
 from flask import request
-from flask.views import MethodView, ResponseReturnValue
+from flask.views import MethodView
+from flask.typing import ResponseReturnValue
 from flask.wrappers import Response
 
 from .database.auth import get_user_by_id, update_last_update_zoom
@@ -56,9 +57,12 @@ class ApiZoomS3UploadView(APIView):
 
     def get_managers_zooms(self, user) -> pd.Series:
         managers_zooms = self.load_dataframe(self.managers_zooms_path)
-        managers_zooms['datetime'] = pd.to_datetime(managers_zooms.date) + pd.to_timedelta(
-            managers_zooms.time.astype(str))
-        managers_zooms = managers_zooms[(managers_zooms.manager == user)].loc[:, 'datetime']
+        managers_zooms["datetime"] = pd.to_datetime(
+            managers_zooms.date
+        ) + pd.to_timedelta(managers_zooms.time.astype(str))
+        managers_zooms = managers_zooms[(managers_zooms.manager == user)].loc[
+            :, "datetime"
+        ]
         return managers_zooms
 
     @staticmethod
@@ -66,36 +70,34 @@ class ApiZoomS3UploadView(APIView):
         zoom_timeframes: List[Tuple] = []
         zoom_time = managers_zooms.to_numpy()
         for i in range(len(zoom_time)):  # np.datetime64 2023-07-28T10:00:00.000000000
-            cur_zt = zoom_time[i] - np.timedelta64(5, 'm')
+            cur_zt = zoom_time[i] - np.timedelta64(5, "m")
             next_zt = zoom_time[i + 1] if i < len(zoom_time) - 1 else zoom_time[i]
-            next_zt = next_zt - np.timedelta64(5, 'm')
+            next_zt = next_zt - np.timedelta64(5, "m")
 
             # если между текущим временем и следующем меньше дня
-            time_duration = np.timedelta64(23, 'h') + np.timedelta64(59, 'm') + np.timedelta64(59, 's')
+            time_duration = (
+                np.timedelta64(23, "h")
+                + np.timedelta64(59, "m")
+                + np.timedelta64(59, "s")
+            )
             if (
-                    np.datetime64(next_zt, 'D') - np.datetime64(cur_zt, 'D')
-            ) / np.timedelta64(1, 'D') == 0:
+                np.datetime64(next_zt, "D") - np.datetime64(cur_zt, "D")
+            ) / np.timedelta64(1, "D") == 0:
                 if cur_zt != next_zt:
-                    zoom_timeframes.append(
-                        (zoom_time[i], cur_zt, next_zt)
-                    )
+                    zoom_timeframes.append((zoom_time[i], cur_zt, next_zt))
                 else:
-                    next_zt = np.datetime64(cur_zt, 'D') + time_duration
-                    zoom_timeframes.append(
-                        (zoom_time[i], cur_zt, next_zt)
-                    )
+                    next_zt = np.datetime64(cur_zt, "D") + time_duration
+                    zoom_timeframes.append((zoom_time[i], cur_zt, next_zt))
             else:
                 # делаю след время до 23:59:59 и добавляю в zoom_timeframes
-                next_zt = np.datetime64(cur_zt, 'D') + time_duration
-                zoom_timeframes.append(
-                    (zoom_time[i], cur_zt, next_zt)
-                )
+                next_zt = np.datetime64(cur_zt, "D") + time_duration
+                zoom_timeframes.append((zoom_time[i], cur_zt, next_zt))
         return zoom_timeframes
 
     @staticmethod
-    def check_date_include(zt: np.datetime64,
-                           zt_start: np.datetime64,
-                           zt_end: np.datetime64):
+    def check_date_include(
+        zt: np.datetime64, zt_start: np.datetime64, zt_end: np.datetime64
+    ):
         return zt_start < zt < zt_end
 
     def post(self):
@@ -104,10 +106,12 @@ class ApiZoomS3UploadView(APIView):
         tz_msk = 3
         uploaded_files = []
 
-        manager_id = session.get('uid')
+        manager_id = session.get("uid")
         manager = get_user_by_id(manager_id).username
-        s3_files = request.values.to_dict().pop("s3_files").split(',')
-        currentTimeZoneOffsetInHours = request.values.to_dict().pop("currentTimeZoneOffsetInHours")
+        s3_files = request.values.to_dict().pop("s3_files").split(",")
+        currentTimeZoneOffsetInHours = request.values.to_dict().pop(
+            "currentTimeZoneOffsetInHours"
+        )
         managers_zooms = self.get_managers_zooms(user=manager)
 
         # for dir, file_list in request.files.lists():
@@ -117,7 +121,9 @@ class ApiZoomS3UploadView(APIView):
         # если имеется информация по zoom по данному пользователю
         # формирую временные отрезки для последующего поиска нужной zoom конференции
         if managers_zooms.count().sum() == 0:
-            self.data = json.dumps({'status_upload': 'failed', 'message': 'ManagerNotFoundError'})
+            self.data = json.dumps(
+                {"status_upload": "failed", "message": "ManagerNotFoundError"}
+            )
             return super().post()
         else:
             zoom_timeframes = self.get_zoom_timeframes(managers_zooms)
@@ -127,51 +133,66 @@ class ApiZoomS3UploadView(APIView):
             for path, list_files in request.files.lists():
                 data_in_path = path[:19]
                 if self.check_date_format(data_in_path, date_format):
-                    datetime64_obj = np.datetime64(data_in_path.replace('.', ':'))
+                    datetime64_obj = np.datetime64(data_in_path.replace(".", ":"))
                     # привожу к utc и +3 мск
-                    datetime64_obj = datetime64_obj + np.timedelta64(int(currentTimeZoneOffsetInHours) + tz_msk, 'h')
+                    datetime64_obj = datetime64_obj + np.timedelta64(
+                        int(currentTimeZoneOffsetInHours) + tz_msk, "h"
+                    )
                     for zt_base, zt_start, zt_end in zoom_timeframes:
                         if self.check_date_include(datetime64_obj, zt_start, zt_end):
                             for file in list_files:
-                                zt_base = np.datetime_as_string(np.datetime64(zt_base), unit='s')
-                                zt_base = datetime.strptime(zt_base, "%Y-%m-%dT%H:%M:%S")
+                                zt_base = np.datetime_as_string(
+                                    np.datetime64(zt_base), unit="s"
+                                )
+                                zt_base = datetime.strptime(
+                                    zt_base, "%Y-%m-%dT%H:%M:%S"
+                                )
 
                                 file_path = os.path.join(
                                     manager,
                                     zt_base.strftime(save_date_format),
                                     path,
-                                    file.filename).replace('\\', '/'
-                                                           )
+                                    file.filename,
+                                ).replace("\\", "/")
 
                                 if file_path not in s3_files:
                                     tmp_file_dir = os.path.join(
                                         tmpdir,
                                         manager,
                                         zt_base.strftime(save_date_format),
-                                        path
+                                        path,
                                     )
 
                                     os.makedirs(tmp_file_dir, exist_ok=True)
                                     file.save(os.path.join(tmp_file_dir, file.filename))
-                                    uploaded_files.append(f'{manager}-..-{file.filename}')
+                                    uploaded_files.append(
+                                        f"{manager}-..-{file.filename}"
+                                    )
             try:
                 s3_client.put(os.path.join(tmpdir, manager))
                 update_last_update_zoom(manager_id)
-                self.data = json.dumps({'status_upload': 'ok', 'cloudfiles': uploaded_files})
+                self.data = json.dumps(
+                    {"status_upload": "ok", "cloudfiles": uploaded_files}
+                )
             except FileNotFoundError as err:
-                self.data = json.dumps({'status_upload': 'failed', 'message': 'FileNotFoundError'})
+                self.data = json.dumps(
+                    {"status_upload": "failed", "message": "FileNotFoundError"}
+                )
 
         return super().post()
 
 
 class ApiZoomS3GetUserFilesView(APIView):
     def post(self):
-        manager_id = session.get('uid')
+        manager_id = session.get("uid")
         manager_name = get_user_by_id(manager_id).username
         s3_client = Client()
         user_files = s3_client.walk(manager_name)
-        self.data = json.dumps({'cloudfiles': user_files}, indent=2, ensure_ascii=False).encode('utf-8')
+        self.data = json.dumps(
+            {"cloudfiles": user_files}, indent=2, ensure_ascii=False
+        ).encode("utf-8")
         return super().post()
+
 
 # class TestView(APIView):
 #     def get(self, *args, **kwargs):
