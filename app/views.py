021d3636e1785ab7bb703bda90371bbc8e25814c
Максимов Юрий <bl146u@gmail.com>
2023-08-21 18:45:08 +0700

    API-метод для определения весов квиза

--- app/views.py
+++ app/views.py
@@ -3869,20 +3869,17 @@ class TildaQuizWeightView(APIView):
         else:
             tag = "1 очередь"
         self.data = {"weigh": weight, "tag": tag}
-        tags = [
-            {
-                "id": lead.get("id"),
-                "_embedded": {
-                    "tags": lead.get("tags", []) + [{"name": tag}],
-                },
-            }
-        ]
+        tags = {
+            "_embedded": {
+                "tags": lead.get("tags", []) + [{"name": tag}],
+            },
+        }
         amocrm_api = AmoCRMAPI()
-        amocrm_api("/api/v4/leads", tags)
-        print('-----------------------------')
+        amocrm_api(f'leads/{lead.get("id")}', tags)
+        print("-----------------------------")
         print(amocrm_api.error)
         print(amocrm_api.response)
-        print('-----------------------------')
+        print("-----------------------------")
         print(json.dumps(tags, indent=2, ensure_ascii=False))
         return super().post(*args, **kwargs)
 
