1169525750f281ed063533f74679c36e7bf87f46
Кирилл <kiryamiass7@gmail.com>
2023-10-04 20:27:07 +0500

    обход бага "кривых дней" sipuni calls

--- app/ats/api/load.py
+++ app/ats/api/load.py
@@ -1,29 +1,57 @@
-import os
-from datetime import datetime
+import datetime
+import hashlib
+from io import BytesIO
 import requests
-from app.ats.api.config import create_config
-import asyncio
+import pandas
+
+from app.ats.api.config import hash_api, user_api, url_api
 
 
-# функция обращается по api и получает csv файл
-async def get_data(data):
-    api_url = 'https://sipuni.com/api/statistic/export'
-    try:
-        response = requests.post(api_url, data=data)
-        if response.status_code == 200:
-            with open(f"app/ats/api/data.csv", 'wb') as file:
-                file.write(response.content)
-                return response.status_code
-        else:
-            with open('app/ats/api/api.log', 'a', encoding='utf-8') as file:
-                file.write(f'{datetime.now()} {response.status_code}\n')
-                return response.status_code
-    except Exception as e:
-        with open('app/ats/api/api.log', 'a', encoding='utf-8') as file:
-            file.write(f'{datetime.now()} {str(e)}\n')
+def parse_date(value: str) -> datetime.date:
+    return datetime.date.fromisoformat(value)
+
+
+def get_response(df: datetime.date, dt: datetime.date) -> pandas.DataFrame:
+    hash_dict = {
+        'anonymous': '1',
+        'dtmfUserAnswer': '0',
+        'firstTime': '0',
+        'from': datetime.date.strftime(df, '%Y-%m-%d'),
+        'fromNumber': '',
+        'names': '1',
+        'numbersInvolved': '1',
+        'numbersRinged': '1',
+        'outgoingLine': '1',
+        'showTreeId': '0',
+        'state': '0',
+        'to': datetime.date.strftime(dt, '%Y-%m-%d'),
+        'toAnswer': '',
+        'toNumber': '',
+        'tree': '000-478278',
+        'type': '2',
+        'user': user_api,
+        'hash': hash_api,
+    }
+    hash_string = '+'.join([hash_dict.get(key, '') for key in hash_dict])
+    hash = hashlib.md5(hash_string.encode()).hexdigest()
+    hash_dict.update({'hash': hash})
+    response = requests.post(url_api, data=hash_dict)
+    response_df: pandas.DataFrame = pandas.read_csv(BytesIO(response.content), delimiter=';', encoding='utf-8',
+                                                    header=0)
+    return response_df
 
 
 def start_import(from_date, to_date):
-    data = create_config(from_date, to_date)
-    result = asyncio.run(get_data(data))
-    return result
+    df = datetime.datetime.strptime(from_date, '%d-%m-%Y')
+    dt = datetime.datetime.strptime(to_date, '%d-%m-%Y')
+    selected_date = df
+    tmp = pandas.DataFrame()
+    while selected_date <= dt:
+        try:
+            df = get_response(selected_date, selected_date)
+            tmp = pandas.concat([tmp, df], ignore_index=True)
+            selected_date += datetime.timedelta(days=1)
+        except Exception as e:
+            selected_date += datetime.timedelta(days=1)
+    tmp.to_csv('app/ats/api/data.csv', sep=';', encoding='utf-8')
+    return 200
